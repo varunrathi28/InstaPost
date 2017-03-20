9b479a09e0d6d1141b50e16846bbd979d5d5b297
Varun Rathi <varunrathi28@gmail.com>
2017-03-20 12:40:25 +0530

    Tableview for post added

---
+++ InstaPost/CircleImageView.swift
@@ -0,0 +1,35 @@
+
+//
+//  CircleImageView.swift
+//  InstaPost
+//
+//  Created by Varun Rathi on 18/03/17.
+//  Copyright © 2017 vrat28. All rights reserved.
+//
+
+import UIKit
+
+class CircleImageView: UIImageView {
+
+   override func awakeFromNib()
+    {
+        super.awakeFromNib()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        
+        layer.cornerRadius = self.frame.height/2
+        
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
